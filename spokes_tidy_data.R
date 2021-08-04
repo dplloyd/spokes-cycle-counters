e9c4d7f9ce5d2c711775792093a426369da5e1ab
diarmuid_lloyd <diarmuid.lloyd@gmail.com>
2021-08-04 22:42:34 +0100

    Adds database

--- spokes_tidy_data.R
+++ spokes_tidy_data.R
@@ -20,29 +20,46 @@
 
 ## Packages
 library(tidyverse)
+library(dbplyr)
+library(janitor)
+library(DBI)
 
+# Create SQLite database in which we'll store our data
+ecc_counter_db_file <- "data/ecc-counter-database-output.sqlite"
+ecc_counter_db <- DBI::dbConnect(drv = RSQLite::SQLite(),dbname =  ecc_counter_db_file)
 
-test <- read.csv("data/01 Peffermill Road/bin_2010-01-01_365d.csv")
-
-glimpse(test)
-
+# Gets all the sub-dir names containing csv files which start with bin_.
 list_of_dirs <-
   dir("data/", full.names = TRUE, recursive = TRUE) %>%
   str_subset(".csv") %>%
   str_subset("bin_") %>% 
   as.list()
 
-read_counter_data <- function(path) {
-  df <- read_csv(path) %>%
-    select(Sdate,
-           LaneDirection,
-           Volume,
-           flag = Flags,
-           flag_text = `Flag Text`)
+# separates out some identifying information on each counter station from filenames and paths
+counters <- tibble(path = dir("data/", recursive = TRUE) %>%
+                     str_subset(".csv") %>%
+                     str_subset("bin_")) %>% 
+  separate(col = path, into = c("station_name","filename"), sep = "/", remove = FALSE) %>% 
+  separate(col= station_name, c("station_num","station_name"), sep = "\\s", extra = "merge")
   
+# Carry out some checks of counters
+counters %>% count(station_name) 
+
+
+# Function for reading the columns of (probable) interest and use.
+add_to_db <- function(path) {
+  df <- read_csv(
+    path,
+    col_types = cols(
+      `Flag Text` = col_character(),
+      `LaneDescription` = col_character(),
+      `#Bins` = col_skip(),
+      Bins = col_skip()
+    )
+  )
 }
 
-all_data <- map_df(list_of_dirs,
+all_data <- map_df(list_of_dirs[1:100],
             read_csv,
             col_types = cols(`Flag Text` = col_character(),
                              `LaneDescription` = col_character(),
@@ -52,3 +69,8 @@ all_data <- map_df(list_of_dirs,
 all_data <- all_data %>% mutate(DirectionDescription = as_factor(DirectionDescription))
 
 all_data %>% count(DirectionDescription)
+
+
+
+
+
