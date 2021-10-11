c37214fc36c2495751dadb147df8e7ac76800f5f
diarmuid_lloyd <diarmuid.lloyd@gmail.com>
2021-10-11 21:52:10 +0100

    Separates out bin and pvr files
    Bin and pvr file are now written to two separate tables

--- spokes_build_database.R
+++ spokes_build_database.R
@@ -26,25 +26,25 @@ library(DBI)
 
 # Create SQLite database in which we'll store our data
 ecc_counter_db_file <- "data/ecc-counter-database-output.sqlite"
-ecc_counter_db <- DBI::dbConnect(drv = RSQLite::SQLite(),dbname =  ecc_counter_db_file)
+ecc_counter_db <- DBI::dbConnect(drv = RSQLite::SQLite(), dbname =  ecc_counter_db_file)
 
-# Gets all the sub-dir names containing csv files which start with bin_.
-list_of_dirs <-
-  dir("data/", full.names = TRUE, recursive = TRUE) %>%
-  str_subset(".csv") %>%
-  str_subset("bin_") 
 
-list_of_dirs_pvr <-
-  dir("data/", full.names = TRUE, recursive = TRUE) %>%
-  str_subset(".csv") %>%
-  str_subset("pvr_") 
-
-list_of_dirs <- c(list_of_dirs,list_of_dirs_pvr)
+# Function reads in data from across folders and writes to table.
+# Table distinguished by the leading prefix of filename, which must be supplied.
+# prefix  =   pvr or bin
+# ecc_counter_db = ecc_counter_db defined above
+write_cycle_count_db <- function(prefix, ecc_counter_db){
+  
+  # Gets all the sub-dir names containing csv files which start with bin_.
+  list_of_dirs<-
+    dir("data/", full.names = TRUE, recursive = TRUE) %>%
+    str_subset(".csv") %>%
+    str_subset(paste0(prefix,"_")) 
 
 # separates out some identifying information on each counter station from filenames and paths
 counters <- tibble(path = dir("data/", recursive = TRUE) %>%
                      str_subset(".csv") %>%
-                     str_subset("bin_")) %>% 
+                     str_subset(paste0(prefix,"_"))) %>% 
   separate(col = path, into = c("station_name","filename"), sep = "/", remove = FALSE) %>% 
   separate(col= station_name, c("station_num","station_name"), sep = "\\s", extra = "merge")
   
@@ -52,7 +52,7 @@ counters <- tibble(path = dir("data/", recursive = TRUE) %>%
 counters %>% count(station_name) 
 
 
-
+# Deal with the bin files first
 #do as a loop, as purrr resulting in memory issues
 first_run <- TRUE
 for (i in 1:length(list_of_dirs)) {
@@ -80,18 +80,41 @@ for (i in 1:length(list_of_dirs)) {
            filename = counters$filename[i])
   
   if (first_run == TRUE) {
-    dbWriteTable(ecc_counter_db, "station_counts_hour", df, overwrite = TRUE)
+    dbWriteTable(ecc_counter_db, paste0("station_counts_hour_",prefix), df, overwrite = TRUE)
     first_run = FALSE
   }
   else {
-    dbWriteTable(ecc_counter_db, "station_counts_hour", df, append = TRUE)
+    dbWriteTable(ecc_counter_db, paste0("station_counts_hour_",prefix), df, append = TRUE)
   }  
   
   rm(df)
+  
+  
 }
 
-# Disconnect
+}
+
+write_cycle_count_db("bin",  ecc_counter_db)
+write_cycle_count_db("pvr",  ecc_counter_db)
+
+
 dbDisconnect(ecc_counter_db)
+
+# Merging the two datasets
+
+# Load the data
+
+con <- DBI::dbConnect(RSQLite::SQLite(), dbname = "data/ecc-counter-database-output.sqlite")
+
+df_bin <- tbl(con, "station_counts_hour_bin") %>% collect()
+
+df_pvr <- tbl(con, "station_counts_hour_pvr") %>% collect()
+
+dbDisconnect(con)
+
+
+df_bin <- df_bin %>% group_by(date, direction_description)
+df_pvr <- df_pvr %>% group_by(date, direction_description)
 
 
 
